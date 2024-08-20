147f547278a45f669cdd4205ecee9ed347ac44c6
DanielGlick45 <danielglck51@gmail.com>
2024-08-20 12:07:29 -0400

    Add button for adding suggest box

--- SampleApp/AutoSuggestBoxSamples/TabView.xaml.cs
+++ SampleApp/AutoSuggestBoxSamples/TabView.xaml.cs
@@ -6,29 +6,29 @@ namespace SampleApp.AutoSuggestBoxSamples;
 [SamplePriority(9)]
 public partial class TabView : ContentPage
 {
-	public TabView()
-	{
-		InitializeComponent();
-		Initialize();
-	}
+    public TabView()
+    {
+        InitializeComponent();
+        Initialize();
+    }
 
+    AutoSuggestBox SuggestBox1;
     List<string> countries;
-	void Initialize()
+    void Initialize()
     {
         using (var s = typeof(TabView).Assembly.GetManifestResourceStream("SampleApp.Data.Countries.txt"))
             countries = new StreamReader(s).ReadToEnd().Split('\n').Select(t => t.Trim()).ToList();
 
-         SuggestBox1.ItemsSource = countries;
         SuggestBox2.ItemsSource = countries;
     }
 
-	void SuggestBox_TextChanged(object sender, AutoSuggestBoxTextChangedEventArgs e)
+    void SuggestBox_TextChanged(object sender, AutoSuggestBoxTextChangedEventArgs e)
     {
         AutoSuggestBox box = (AutoSuggestBox)sender;
         // Filter the list based on text input
-        
+
         var suggestions = GetSuggestions(box.Text);
-        if(suggestions.Count > 0)
+        if (suggestions.Count > 0)
             box.IsSuggestionListOpen = true;
 
         box.ItemsSource = suggestions;
@@ -37,5 +37,13 @@ public partial class TabView : ContentPage
     List<string> GetSuggestions(string text)
     {
         return string.IsNullOrWhiteSpace(text) ? countries : countries.Where(s => s.StartsWith(text, StringComparison.InvariantCultureIgnoreCase)).ToList();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        SuggestBox1 = new AutoSuggestBox() { ItemsSource = countries };
+        SuggestBox1.TextChanged += SuggestBox_TextChanged;
+
+        uiGrid2.Children.Add(SuggestBox1);
     }
 }
